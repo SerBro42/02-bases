cf247e5d87f828459069a4ad8c63ae5ccca7b570
Sergey Brovko <serbro7@gmail.com>
2024-06-06 09:48:18 +0200

    changed directory of list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './list/list/list.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
     HeroComponent,
-    ListComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
