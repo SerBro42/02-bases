970b3c74490e785c0f69a383a182366cb673df00
Sergey Brovko <serbro7@gmail.com>
2024-06-06 09:19:10 +0200

    counter functionalities completely transferred to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   public title: string = 'Hola Mundo';
-  public counter: number = 10;
-
-  increaseBy( value: number ):void{
-    this.counter += value;
-  }
-
-  reset():void{
-    if(this.counter != 10){
-      this.counter = 10;
-    }
-  }
 
   // decreaseBy():void{
   //   this.counter -=1;
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,11 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter</h1>'
+  template: `
+
+  <h3>
+    Counter: {{ counter }}
+  </h3>
+  <button (click)="increaseBy(+1)">
+    +1
+  </button>
+  <button (click)="reset()">
+    Reset
+  </button>
+  <button (click)="increaseBy(-1)">
+    -1
+  </button>
+
+  `
 })
 
 export class CounterComponent {
   constructor() { }
+
+  public counter: number = 10;
+
+  increaseBy( value: number ):void{
+    this.counter += value;
+  }
+
+  reset():void{
+    if(this.counter != 10){
+      this.counter = 10;
+    }
+  }
 
 
 }
